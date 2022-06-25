8c670034eb2b211d143e368e338f46a496afec93
JeffersonRolino <jefferson.rolino@gmail.com>
2022-06-25 13:12:08 -0300

    First commit with the boilerplate of the project

---
+++ src/main/java/jade/MouseListener.java
@@ -0,0 +1,28 @@
+package jade;
+
+public class MouseListener {
+    private static MouseListener instance;
+    private double scrollX, scrollY;
+    private double xPos, yPos, lastX, lastY;
+    private boolean mouseButtonPressed[] = new boolean[3];
+    private boolean isDragging;
+
+    private MouseListener(){
+        this.scrollX = 0;
+        this.scrollY = 0;
+        this.xPos = 0;
+        this.yPos = 0;
+        this.lastX = 0;
+        this.lastY = 0;
+    }
+
+    public static MouseListener get(){
+        if(MouseListener.instance == null){
+            MouseListener.instance = new MouseListener();
+        }
+
+        return MouseListener.instance;
+    }
+
+
+}
